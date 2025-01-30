1a49bafa86914e01938a391fa0f015913375e98a
cnHealth <cnhealth24@gmail.com>
2025-01-30 15:26:30 -0300

    Adicionado toolbar

--- app/src/main/java/com/mjtech/mentoria/favoriteAdvice/presentation/FavoritesAdviceScreen.kt
+++ app/src/main/java/com/mjtech/mentoria/favoriteAdvice/presentation/FavoritesAdviceScreen.kt
@@ -1,12 +1,17 @@
 package com.mjtech.mentoria.favoriteAdvice.presentation
 
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import com.mjtech.mentoria.favoriteAdvice.domain.AdviceEntity
 import com.mjtech.mentoria.favoriteAdvice.presentation.components.FavoritesAdviceContent
 import com.mjtech.mentoria.favoriteAdvice.presentation.state.FavoriteAdviceState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavoritesAdviceScreen(
     uiState: FavoriteAdviceState,
@@ -17,6 +22,16 @@ fun FavoritesAdviceScreen(
 
 
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Favoritos",
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                },
+            )
+        },
         content = { paddingValues ->
             FavoritesAdviceContent(
                 paddingValues,
