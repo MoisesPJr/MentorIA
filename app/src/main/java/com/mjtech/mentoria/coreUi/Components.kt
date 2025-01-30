aa864e00212dc00b47e11e05f769cad94de4bc72
cnHealth <cnhealth24@gmail.com>
2025-01-30 16:08:49 -0300

    Adicionado padding na lista de favoritos

--- app/src/main/java/com/mjtech/mentoria/coreUi/Components.kt
+++ app/src/main/java/com/mjtech/mentoria/coreUi/Components.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
@@ -59,7 +60,7 @@ fun AdvicesListScreen(
 
     val localContext = LocalContext.current
 
-    Box {
+    Box(modifier = Modifier.padding(16.dp)) {
         LazyColumn(
             modifier = modifier,
             verticalArrangement = Arrangement.spacedBy(8.dp),
